54b2a33a41a6754f640603b0c028956ed3d03036
christianmvd <86528914+christianmvd@users.noreply.github.com>
2022-03-28 16:58:41 -0300

    primera subida en heroku

--- components/users/UsersLogin.js
+++ components/users/UsersLogin.js
@@ -5,19 +5,15 @@ const bcrypt = require('bcrypt');
 const generateToken = require('../../utils/generateToken');
 const authenticateToken = require('../../utils/authenticateToken');
 const UsersRegistro = require('./UsersRegistro');
-const logger = require('../../utils/winston')
-
 
 module.exports = (app) => {
   app.use('/login', router);
 
   router.get('/', (req, res) => {
-    logger.log('info', `ruta /login, metodo get`);
     res.render('login');
   });
 
   router.post('/', async (req, res) => {
-    logger.log('info', `ruta /login, metodo post`);
     const { mail, password } = req.body;
     if (!mail || !mail.length) {
       res.status(401).send();
@@ -25,13 +21,22 @@ module.exports = (app) => {
     }
     try {
       const user = await Users.find({ mail: mail });
+     
+      if (user.length == 0)  
+        user.push({password: ''})    //si la busqueda del email no existe pongo una clave vacia para que de error la verificacion de la clave de abajo
+      
       const confirmPassword = await bcrypt.compare(password, user[0].password);
+      
       if (!confirmPassword) {
+        console.log('Error, clave incorrecta')
         return res.render('login-error');
+
       }
       const access_token = generateToken(user);
 
-      res.json({ access_token })
+      //res.json({ access_token })
+      //res.json(console.log('autenticacion correcta!'))
+      return res.render('index',{mail: res.mail});
     } catch (error) {
       console.log(error);
     }
