129b84aadd2529fb9b903b4d7520b76ec8c68d29
christianmvd <86528914+christianmvd@users.noreply.github.com>
2022-03-28 16:44:08 -0300

    primera subida

---
+++ public/login.js
@@ -0,0 +1,22 @@
+const submitBtn = document.querySelector('.submit')
+const mailInput = document.querySelector('[name="mail"]')
+const passwordInput = document.querySelector('[name="password"]')
+
+
+submitBtn.addEventListener('click', async (e)=>{  
+  e.preventDefault()
+  const mail = mailInput.value
+  const password = passwordInput.value
+  const res = await fetch('http://localhost:8080/login', {
+    method = 'POST',
+    body: {
+      mail,
+      password
+    }
+  })
+  const data = await res.json()
+
+  await fetch('http://localhost:8080/')
+  console.log(data)
+  
+})
